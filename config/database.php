7b965df7ebf955262b5c45a5709adef86e722cc7
Max <smy980807@ukr.net>
2018-09-17 21:53:59 +0300

    port config

--- config/database.php
+++ config/database.php
@@ -1,7 +1,8 @@
 <?php
 
     $DB_HOST = "127.0.0.1";
+    $DB_PORT = "3308";
     $DB_NAME = "camagru";
     $DB_USER = "root";
     $DB_PASSWORD = "123456";
-    $DB_DSN = "mysql:host=$DB_HOST;dbname=$DB_NAME;port=3308";
+    $DB_DSN = "mysql:host=$DB_HOST;dbname=$DB_NAME;port=$DB_PORT";
