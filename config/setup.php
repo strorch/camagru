3fbed88c075a5f88a2a1b8157fad8b7d467cee47
strorch <smy980807@ukr.net>
2019-09-05 02:12:35 +0300

    fixed pathes

--- config/setup.php
+++ config/setup.php
@@ -1,42 +1,10 @@
 <?php
 
-require_once '../src/core/DB.php';
+require_once __DIR__.'/../src/bootstrap.php';
 
-define('ROOTPATH', __DIR__.'/..');
-
-$connection = new DB();
-
+$connection = new \core\DB();
+var_dump($connection);
+die();
 $query_str = file_get_contents('./setup.sql');
-
 $connection->exec($query_str);
-
-
-$to_stikers = "./public/stikers/";
-$return[] = "mem1.jpg";
-$return[] = "mem2.jpeg";
-$return[] = "mem3.jpg";
-$return[] = "mem4.jpg";
-$return[] = "mem5.jpg";
-$return[] = "mem6.jpg";
-$return[] = "mem7.jpg";
-
-
-foreach ($return as $value)
-{
-    echo "KEKEKEKE     ".$value;
-    $connection->exec("INSERT INTO posts (\"USER\", PICT) VALUES ('".$value."', '".$to_stikers.$value."');");
-}
 echo "done";
-
-
-//require_once '../serv/app/Parent/DB_connection.php';
-//
-//define('ROOTPATH', __DIR__ . '/..');
-//
-//$connection = new DBConnection();
-//
-//$query_str = file_get_contents('./setup.sql');
-//
-//$connection->exec($query_str);
-//
-//echo "done";
--- src/bootstrap.php
+++ src/bootstrap.php
@@ -2,7 +2,7 @@
 
 error_reporting(E_ALL);
 
-define('BASE_DIR', $_SERVER['DOCUMENT_ROOT']);
+define('BASE_DIR', $_SERVER['DOCUMENT_ROOT'] === '' ? $_SERVER['PWD'] : $_SERVER['DOCUMENT_ROOT']);
 define('SRC_DIR', BASE_DIR.'/src');
 define('CONTROLLERS_DIR', SRC_DIR.'/controllers');
 define('ASSETS_DIR', BASE_DIR.'/assets');
