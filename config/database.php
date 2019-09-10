034a9b07a3e88d46cd30de3a2dd4564eb93e270e
strorch <smy980807@ukr.net>
2019-09-10 17:27:25 +0300

    minor

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 return [
     'type' => 'pgsql',
-    'host' => "192.168.32.2",
+    'host' => "192.168.48.2", //TODO: https://hub.docker.com/r/diouxx/apache-proxy add proxy to configure stable ip
 //    'host' => "172.24.0.2",
     'port' => '5432',
     'dbName' => 'postgres',
