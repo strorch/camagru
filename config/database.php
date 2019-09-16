e138dccce5669f7f04ba88e49fdc2364ba4b63e9
strorch <smy980807@ukr.net>
2019-09-16 21:10:19 +0300

    conflict fixed

--- config/database.php
+++ config/database.php
@@ -2,7 +2,7 @@
 
 return [
     'type' => 'pgsql',
-    'host' => "192.168.99.102", //TODO: https://hub.docker.com/r/diouxx/apache-proxy add proxy to configure stable ip
+    'host' => "172.20.0.2", //TODO: https://hub.docker.com/r/diouxx/apache-proxy add proxy to configure stable ip
     'port' => '5432',
     'dbName' => 'postgres',
     'user' => 'postgres',
