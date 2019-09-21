8966ffcb5d4d42d0f894dc06947e25f9e5f0db90
strorch <smy980807@ukr.net>
2019-09-21 20:56:55 +0300

    added materialize css

--- src/blades/header.php
+++ src/blades/header.php
@@ -17,6 +17,7 @@ print_r($_SESSION);
     <meta charset="UTF-8">
     <meta name="csrf-token" content="<?= $_SESSION['_csrf'] ?>">
     <title>camagru</title>
+    <link rel="stylesheet" href="/assets/css/materialize.min.css">
     <link rel="stylesheet" href="/assets/css/profile.css?<?=time()?>">
     <link rel="stylesheet" href="/assets/css/front_page.css?<?=time()?>">
     <link rel="stylesheet" href="/assets/css/login.css?<?=time()?>">
