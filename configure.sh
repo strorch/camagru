685aeeca9bd4f6632c9c701a57cfa8ea78d195e6
strorch <smy980807@ukr.net>
2019-09-23 18:55:23 +0300

    minor

--- configure.sh
+++ configure.sh
@@ -7,5 +7,5 @@ docker-compose exec pgsql apt-get install -y vim
 # Migration
 docker-compose exec web ./config/setup.php
 
-# Run up in localhost:9000
+# Run up in localhost:8000
 # VBoxManage controlvm ddd natpf1 rule1,tcp,,8000,,80
